b5f6a9162247967a64d69853a73a10d5dc567f2b
Kanstantinas Piatrashka <piatrashkakanstantinass@gmail.com>
2024-11-04 01:11:58 +0200

    Setup oauth

--- Shopipy.ApiService/Program.cs
+++ Shopipy.ApiService/Program.cs
@@ -1,3 +1,8 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.BearerToken;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Shopipy.ApiService.Data;
 
@@ -11,18 +16,67 @@ builder.Services.AddProblemDetails();
 
 builder.AddNpgsqlDbContext<AppDbContext>("postgresdb");
 
+// Life is fun with aspire and all that stuff,
+// but I need a debugger
+// builder.Services.AddDbContext<AppDbContext>(options =>
+// {
+//     options.UseInMemoryDatabase("db");
+// });
+
 builder.Services.AddControllers();
 
+builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddIdentityCore<IdentityUser>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddSignInManager();
+
+builder.Services.AddAuthentication(BearerTokenDefaults.AuthenticationScheme)
+    .AddBearerToken()
+    .AddGoogle(options =>
+    {
+        var config = builder.Configuration.GetSection("Authentication:Google");
+        options.ClientId = config["ClientId"]!;
+        options.ClientSecret = config["ClientSecret"]!;
+        options.Events.OnTicketReceived = async (context) =>
+        {
+            var providerKey = context.Principal?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var emailAddress = context.Principal?.FindFirst(ClaimTypes.Email)?.Value;
+            if (providerKey == null) throw new InvalidOperationException("nameidentifier not found");
+            if (emailAddress == null) throw new InvalidOperationException("email not found");
+            
+            var userManager = context.HttpContext.RequestServices.GetRequiredService<UserManager<IdentityUser>>();
+            var user = await userManager.FindByLoginAsync(GoogleDefaults.AuthenticationScheme, providerKey);
+            if (user == null)
+            {
+                user = new IdentityUser(emailAddress);
+                await userManager.CreateAsync(user);
+                await userManager.AddLoginAsync(user, new UserLoginInfo(GoogleDefaults.AuthenticationScheme, providerKey, null));
+            }
+
+            var signInManager = context.HttpContext.RequestServices.GetRequiredService<SignInManager<IdentityUser>>();
+            var principal = await signInManager.CreateUserPrincipalAsync(user);
+            await context.HttpContext.SignInAsync(principal);
+            context.HandleResponse();
+        };
+        
+    });
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseExceptionHandler();
 
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapDefaultEndpoints();
 
 app.MapControllers();
+
+app.MapGet("oauth-google", () => Results.Challenge(null, [GoogleDefaults.AuthenticationScheme]));
 
 app.UseSwagger();
 app.UseSwaggerUI();
