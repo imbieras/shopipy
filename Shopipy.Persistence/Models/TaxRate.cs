e65ba6ecd2ea3b06f8d7f97808f7c48e2a2f355a
Artiom Hovhannisyan <a_hovhannisyan@wargaming.net>
2024-12-08 11:52:26 +0200

    add TaxRate entity

---
+++ Shopipy.Persistence/Models/TaxRate.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Shopipy.Persistence.Models;
+
+public class TaxRate
+{
+    [Key]
+    public required int TaxRateId { get; set; }
+    public required string Name { get; set; }
+    public required decimal Rate { get; set; }
+    public required DateTime EffectiveFrom { get; set; } 
+    public DateTime? EffectiveTo { get; set; } // null if active
+    public DateTime CreatedAt { get; set; }
+    public required int BusinessId { get; set; }
+}
