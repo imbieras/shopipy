34e21b3377723e503a7e729eb3dbfe19e57b7e51
Kanstantinas Piatrashka <piatrashkakanstantinass@gmail.com>
2024-12-16 20:42:10 +0200

    tax rate id mapping fix

--- Shopipy.OrderManagement/Mappings/OrderMappingProfile.cs
+++ Shopipy.OrderManagement/Mappings/OrderMappingProfile.cs
@@ -13,12 +13,14 @@ public class OrderMappingProfile : Profile
             .ForMember(dest => dest.BusinessId, opt => opt.Ignore())
             .ForMember(dest => dest.OrderId, opt => opt.Ignore())
             .ForMember(dest => dest.UnitPrice, opt => opt.Ignore())
-            .ForMember(dest => dest.CreatedAt, opt => opt.Ignore());
+            .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+            .ForMember(dest => dest.TaxRateId, opt => opt.Ignore());
         
         CreateMap<CreateProductOrderItemRequestDto, ProductOrderItem>()
             .IncludeBase<CreateOrderItemRequestDto, OrderItem>();
         
         CreateMap<CreateServiceOrderItemRequestDto, ServiceOrderItem>()
             .IncludeBase<CreateOrderItemRequestDto, OrderItem>();
+        
     }
 }
